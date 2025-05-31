49230b9844eff4db8e36a634bd9a863e05b4470a
Abhishek Thakur <76610164+dodabhishek@users.noreply.github.com>
2025-05-31 17:36:03 +0530

    render changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+  origin: true,
+  credentials: true,
 }));
 
 app.use(express.json());
